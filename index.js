02776dd68d7dfef4af7d0353c90d977640769836
joshua hahn <tailrin.jh@gmail.com>
2019-09-21 14:17:46 -0600

    add javascript and linke to html

---
+++ index.js
@@ -0,0 +1,36 @@
+$('#js-shopping-list-form').on('submit', event => {
+  event.preventDefault();
+  const value = $('#shopping-list-entry').val()
+
+  let x = `<li>
+        <span class="shopping-item">${value}</span>
+        <div class="shopping-item-controls">
+          <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+          </button>
+          <button class="shopping-item-delete">
+            <span class="button-label">delete</span>
+          </button>
+        </div>
+      </li>`
+  $('.shopping-list').append(x);
+  $('#shopping-list-entry').val('');
+  handleShoppingListClicks();
+});
+
+
+function handleShoppingListClicks(){
+  $('.shopping-item-delete').click(e => {
+    e.preventDefault();
+    $(e.currentTarget).closest('li').remove();
+  });
+
+  $('.shopping-item-toggle').click(e => {
+    e.preventDefault();
+    $(e.currentTarget).closest('li')
+    .toggleClass('shopping-item__checked');
+    
+  });
+} 
+
+handleShoppingListClicks();
