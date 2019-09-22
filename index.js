db99b3ddf88cec4fe596af0e9773d580dc2b80c3
tailrin <51151455+tailrin@users.noreply.github.com>
2019-09-21 20:09:31 -0600

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function handleDeleteClicks(){
 function handleCheckClicks(){
    $('.shopping-item').click(e => {
        e.preventDefault();
-      $(e.currentTarget).siblings().first().toggleClass('shopping-item__checked'); 
+      $(e.currentTarget). parents ().siblings().first().toggleClass('shopping-item__checked'); 
   });
 }
 function handleShoppingListClicks(){
